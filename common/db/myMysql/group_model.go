9365cbff2449bda5e934be3d2eb61d875aa1fb59
n8sPxD <noobsoap233@gmail.com>
2024-10-09 20:59:47 +0800

    实现群聊消息存库逻辑(DAO层逻辑)

--- common/db/myMysql/group_model.go
+++ common/db/myMysql/group_model.go
@@ -10,10 +10,24 @@ import (
 
 func (db *DB) InsertGroup(ctx context.Context, group *models.Group) error {
 	if err := db.client.WithContext(ctx).Create(group).Error; err != nil {
-        if errors.Is(err, gorm.ErrDuplicatedKey) {
-            return gorm.ErrDuplicatedKey
-        }
-        return err
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return gorm.ErrDuplicatedKey
+		}
+		return err
 	}
-    return db.client.WithContext(ctx).Create(&models.GroupConfig{GroupID: uint32(group.ID)}).Error
+	return db.client.WithContext(ctx).Create(&models.GroupConfig{GroupID: uint32(group.ID)}).Error
+}
+
+func (db *DB) GetGroupMembers(ctx context.Context, id uint) ([]uint32, error) {
+	var members models.Group
+	err := db.client.WithContext(ctx).
+		Model(&models.Group{}).
+		Select("group_members").
+		Where("id = ?", id).
+		Take(&members).
+		Error
+	if err != nil {
+		return nil, err
+	}
+	return members.Members, nil
 }
