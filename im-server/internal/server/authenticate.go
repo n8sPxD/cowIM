f31a58f417952067cf8cc4e221140426b6373711
n8sPxD <noobsoap233@gmail.com>
2024-10-11 00:33:11 +0800

    feat: 前端实现连接websocket服务器

--- im-server/internal/server/authenticate.go
+++ im-server/internal/server/authenticate.go
@@ -1,26 +1,16 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/n8sPxD/cowIM/common/jwt"
 )
 
 func (s *Server) authenticate(w http.ResponseWriter, r *http.Request) (*jwt.CustomClaims, bool) {
-	// 先鉴权，再进行消息通讯
-	// 从Authorization头部获取JWT令牌
-	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		http.Error(w, "Authorization header is required", http.StatusUnauthorized)
-		return nil, false
-	}
-
-	// JWT 格式： Bearer <token>
-	var tokenString string
-	fmt.Sscanf(authHeader, "Bearer %s", &tokenString)
+	// 从 URL 查询参数中获取 JWT 令牌
+	tokenString := r.URL.Query().Get("token")
 	if tokenString == "" {
-		http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+		http.Error(w, "Token is required", http.StatusUnauthorized)
 		return nil, false
 	}
 
