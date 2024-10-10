19edf35d719230ad2addeab7de05f2a5c5bc69d1
n8sPxD <noobsoap233@gmail.com>
2024-10-10 16:08:51 +0800

    feat: 实现添加好友接口

--- microservices/user/internal/logic/addfriendslogic.go
+++ microservices/user/internal/logic/addfriendslogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"errors"
 
 	"github.com/n8sPxD/cowIM/microservices/user/internal/svc"
 	"github.com/n8sPxD/cowIM/microservices/user/internal/types"
@@ -23,8 +24,12 @@ func NewAddFriendsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *AddFri
 	}
 }
 
-func (l *AddFriendsLogic) AddFriends(req *types.AddFriendRequest) (resp *types.AddFriendResponse, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+func (l *AddFriendsLogic) AddFriends(req *types.AddFriendRequest) (*types.AddFriendResponse, error) {
+	// 直接调用sql方法
+	err := l.svcCtx.MySQL.InsertFriend(l.ctx, req.UserID, req.FriendID)
+	if err != nil {
+		logx.Error("[AddFriends] Add friend failed, error: ", err)
+		return nil, errors.New("添加好友失败，可能是服务器出了问题")
+	}
+	return nil, nil
 }
