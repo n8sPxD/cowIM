d478fa09df955d8707badc9ebe3a2bc004ddd1f7
n8sPxD <noobsoap233@gmail.com>
2024-10-10 16:55:56 +0800

    自定义response替换好友接口handler

--- microservices/user/internal/handler/getfriendshandler.go
+++ microservices/user/internal/handler/getfriendshandler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/n8sPxD/cowIM/common/response"
 	"github.com/n8sPxD/cowIM/microservices/user/internal/logic"
 	"github.com/n8sPxD/cowIM/microservices/user/internal/svc"
 	"github.com/n8sPxD/cowIM/microservices/user/internal/types"
@@ -13,16 +14,16 @@ func getFriendsHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.GetFriendsRequest
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			response.HttpFail(r, w, err)
 			return
 		}
 
 		l := logic.NewGetFriendsLogic(r.Context(), svcCtx)
 		resp, err := l.GetFriends(&req)
 		if err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			response.HttpFail(r, w, err)
 		} else {
-			httpx.OkJsonCtx(r.Context(), w, resp)
+			response.HttpSuccess(r, w, resp)
 		}
 	}
 }
