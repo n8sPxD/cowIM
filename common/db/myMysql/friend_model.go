69003a59ac636fdea1c1c5874e3edf53adca24b9
n8sPxD <noobsoap233@gmail.com>
2024-10-10 16:49:04 +0800

    fix: 修复查询好友列表数据库层问题

--- common/db/myMysql/friend_model.go
+++ common/db/myMysql/friend_model.go
@@ -16,12 +16,22 @@ func (db *DB) InsertFriend(ctx context.Context, userID, friendID uint32) error {
 
 func (db *DB) GetFriends(ctx context.Context, userID uint32) ([]models.User, error) {
 	var friends []models.User
-	err := db.client.WithContext(ctx).
-		Preload("Friend").
-		Model(&models.Friends{}).
-		Select("friends").
-		Where("user_id", userID).
-		Take(&friends).
+	// 子查询，分别对应自己加别人 和 别人加自己的情况
+	sub1 := db.client.
+		Table("friends").
+		Select("friend_id").
+		Where("user_id = ?", userID)
+	sub2 := db.client.
+		Table("friends").
+		Select("user_id").
+		Where("friend_id = ?", userID)
+
+	err := db.client.
+		Debug().
+		Select("username", "id").
+		Where("id = (?)", sub1).
+		Or("id = (?)", sub2).
+		Find(&friends).
 		Error
 	if err != nil {
 		return nil, err
