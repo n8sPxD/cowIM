6c30151fd24d828b242d724c254f66c9934a5734
n8sPxD <noobsoap233@gmail.com>
2024-10-10 16:05:00 +0800

    好友接口mysql依赖注入

--- microservices/user/internal/config/config.go
+++ microservices/user/internal/config/config.go
@@ -1,7 +1,12 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
+	MySQL struct {
+		DataSource string
+	}
 }
--- microservices/user/internal/svc/servicecontext.go
+++ microservices/user/internal/svc/servicecontext.go
@@ -1,15 +1,18 @@
 package svc
 
 import (
+	"github.com/n8sPxD/cowIM/common/db/myMysql"
 	"github.com/n8sPxD/cowIM/microservices/user/internal/config"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	MySQL  *myMysql.DB
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		MySQL:  myMysql.MustNewMySQL(c.MySQL.DataSource),
 	}
 }
