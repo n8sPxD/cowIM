02330c8cd697a88012b43fbda6e889b61d7c580e
n8sPxD <noobsoap233@gmail.com>
2024-10-10 22:39:24 +0800

    服务发现http微服务实现发现中心依赖注入

--- microservices/wsget/internal/config/config.go
+++ microservices/wsget/internal/config/config.go
@@ -4,4 +4,7 @@ import "github.com/zeromicro/go-zero/rest"
 
 type Config struct {
 	rest.RestConf
+	Etcd struct {
+		Host []string
+	}
 }
--- microservices/wsget/internal/svc/servicecontext.go
+++ microservices/wsget/internal/svc/servicecontext.go
@@ -1,15 +1,18 @@
 package svc
 
 import (
+	"github.com/n8sPxD/cowIM/common/servicehub"
 	"github.com/n8sPxD/cowIM/microservices/wsget/internal/config"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config       config.Config
+	DiscoveryHub *servicehub.DiscoveryHub
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:       c,
+		DiscoveryHub: servicehub.NewDiscoveryHub(c.Etcd.Host, 3),
 	}
 }
