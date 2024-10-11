a1f45c110e247d9fe7bb6028684cf3b98838a93e
n8sPxD <noobsoap233@gmail.com>
2024-10-12 01:23:59 +0800

    更改同步timeline接口逻辑

--- microservices/info/internal/logic/timelinesynclogic.go
+++ microservices/info/internal/logic/timelinesynclogic.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/n8sPxD/cowIM/common/db/myMongo/models"
 	"github.com/n8sPxD/cowIM/microservices/info/internal/svc"
 	"github.com/n8sPxD/cowIM/microservices/info/internal/types"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -24,20 +25,12 @@ func NewTimelineSyncLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Time
 	}
 }
 
-type TimelineSyncInfo struct {
-	SenderID   uint32    `json:"senderId"`
-	ReceiverID uint32    `json:"receiverId"`
-	GroupID    uint32    `json:"groupId,omitempty"`
-	Message    string    `json:"message"`
-	Timestamp  time.Time `json:"timestamp"`
-}
-
 type TimelineSyncResponse struct {
-	Infos []TimelineSyncInfo `json:"infos"`
+	Infos []models.UserTimeline `json:"infos"`
 }
 
 // TimelineSync 以用户本地最新消息为起始点，获取服务器中更新的消息
-func (l *TimelineSyncLogic) TimelineSync(req *types.TimelineSyncRequest) (resp *TimelineSyncResponse, err error) {
+func (l *TimelineSyncLogic) TimelineSync(req *types.TimelineSyncRequest) (*TimelineSyncResponse, error) {
 	// 获取消息
 	chats, err := l.svcCtx.Mongo.GetRecentChatList(l.ctx, req.ID, time.Unix(req.Timestamp, 0))
 	if err != nil {
@@ -45,22 +38,5 @@ func (l *TimelineSyncLogic) TimelineSync(req *types.TimelineSyncRequest) (resp *
 		return nil, errors.New("获取消息失败")
 	}
 
-	logx.Debug("[TimelineSync] chats: ", chats)
-
-	// 将消息封装到resp中
-	infos := make([]TimelineSyncInfo, len(chats))
-	for i, chat := range chats {
-		var info TimelineSyncInfo
-		info.Message = chat.RecentMsg
-		info.SenderID = chat.SenderID
-		info.ReceiverID = chat.ReceiverID
-		info.GroupID = chat.GroupID
-		info.Timestamp = chat.Timestamp
-		infos[i] = info
-	}
-
-	resp = new(TimelineSyncResponse)
-	resp.Infos = infos
-
-	return
+	return &TimelineSyncResponse{Infos: chats}, nil
 }
