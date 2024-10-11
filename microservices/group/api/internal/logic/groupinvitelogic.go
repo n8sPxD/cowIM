063bced6e1b22ed1d9d332ad5a98fbb0a9323918
n8sPxD <noobsoap233@gmail.com>
2024-10-11 16:47:48 +0800

    feat: 实现邀请成员进群接口逻辑

--- microservices/group/api/internal/logic/groupinvitelogic.go
+++ microservices/group/api/internal/logic/groupinvitelogic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"errors"
 
 	"github.com/n8sPxD/cowIM/microservices/group/api/internal/svc"
 	"github.com/n8sPxD/cowIM/microservices/group/api/internal/types"
@@ -23,8 +24,13 @@ func NewGroupInviteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Group
 	}
 }
 
-func (l *GroupInviteLogic) GroupInvite(req *types.GroupInviteRequest) (resp *types.GroupInviteResponse, err error) {
-	// todo: add your logic here and delete this line
-
-	return
+// GroupInvite 拉人进群，暂时定为邀请人就能直接拉进群而不需要别人同意
+func (l *GroupInviteLogic) GroupInvite(req *types.GroupInviteRequest) (*types.GroupInviteResponse, error) {
+	// 直接塞mysql里
+	err := l.svcCtx.MySQL.InsertGroupMembers(l.ctx, req.GroupID, req.Members)
+	if err != nil {
+		logx.Errorf("[GroupInvite] Insert members to group %d failed, error: %v", req.GroupID, err)
+		return nil, errors.New("邀请成员入群失败")
+	}
+	return nil, nil
 }
