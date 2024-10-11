5ee379d047007e95a61880495da070686e4ea500
n8sPxD <noobsoap233@gmail.com>
2024-10-12 00:21:01 +0800

    refactor: 使用自定义响应替换原生handler中响应

--- microservices/group/api/internal/handler/groupinvitehandler.go
+++ microservices/group/api/internal/handler/groupinvitehandler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/n8sPxD/cowIM/common/response"
 	"github.com/n8sPxD/cowIM/microservices/group/api/internal/logic"
 	"github.com/n8sPxD/cowIM/microservices/group/api/internal/svc"
 	"github.com/n8sPxD/cowIM/microservices/group/api/internal/types"
@@ -13,16 +14,16 @@ func groupInviteHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.GroupInviteRequest
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			response.HttpFail(r, w, err)
 			return
 		}
 
 		l := logic.NewGroupInviteLogic(r.Context(), svcCtx)
 		resp, err := l.GroupInvite(&req)
 		if err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			response.HttpFail(r, w, err)
 		} else {
-			httpx.OkJsonCtx(r.Context(), w, resp)
+			response.HttpSuccess(r, w, resp)
 		}
 	}
 }
