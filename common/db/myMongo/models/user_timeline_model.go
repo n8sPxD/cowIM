989c313fa4e35a92cdc15df1a9a6e6332d8d7496
n8sPxD <noobsoap233@gmail.com>
2024-10-13 16:32:49 +0800

    更新Timeline表字段

--- common/db/myMongo/models/user_timeline_model.go
+++ common/db/myMongo/models/user_timeline_model.go
@@ -8,6 +8,7 @@ type UserTimeline struct {
 	ReceiverID uint32      `bson:"receiver_id"        json:"receiverID"`
 	SenderID   uint32      `bson:"sender_id"          json:"senderID"`
 	GroupID    uint32      `bson:"group_id,omitempty" json:"groupID,omitempty"`
+	Type       int8        `bson:"type"               json:"type"`
 	Message    MessageSync `bson:"msgForward"         json:"msgForward"`
 	Timestamp  time.Time   `bson:"timestamp"          json:"timestamp"` // 用于删除过时消息 + 实现Timeline模型(用户消息按时间线排列)
 }
@@ -16,19 +17,20 @@ type UserTimeline struct {
 	单聊:
 		A --我去--> B
 		A.UserID = 233, B.UserID = 666
-		UserID = 233, SenderID = 666
+		SenderID = 233, ReceiverID = 666
 	示例存表, 存B的timeline:
 	{
 		ID: 11451419198, // 使用idgen.NextId()生成
-		UserID: 666,
+		ReceiverID: 666,
 		SenderID: 233,
+        Type: SINGLE_CHAT,
 		Message: MessageSync,
 		Timestamp: time.Now()
 	}
 	MessageSync:
 	{
 		ID: 1,
-		MsgType: 0, // 一般文本消息的常量表示
+		MsgType: COMMON_MSG,
 		Content: "我去"
 		Timestamp: time.Now()
 	}
@@ -43,18 +45,20 @@ type UserTimeline struct {
 	B:
 	{
 		ID: 12345678987,
-		UserID: 666,
 		SenderID: 233,
+		ReceiverID: 666,
 		GroupID: 114514,
+        Type: GROUP_CHAT,
 		Message: MessageSync
 		Timestamp: time.Now()
 	}
 	C:
 	{
 		ID: 12345678988,
-		UserID: 213,
-		SenderID: 233,
+		Sender: 233,
+		ReceiverID: 213,
 		GroupID: 114514,
+        Type: GROUP_CHAT,
 		Message: MessageSync
 		Timestamp: time.Now()
 	}
