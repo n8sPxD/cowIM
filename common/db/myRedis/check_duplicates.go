afe134592acd6577cc8d66eba8f3e63ca78104e4
n8sPxD <noobsoap233@gmail.com>
2024-10-13 17:58:20 +0800

    refactor: 简化代码

--- common/db/myRedis/check_duplicates.go
+++ common/db/myRedis/check_duplicates.go
@@ -13,7 +13,7 @@ func (db *DB) CheckDuplicateMessage(ctx context.Context, uuid string) (bool, err
 	if ok, err = db.SismemberCtx(ctx, key, uuid); err != nil { // redis 出问题了
 		return false, err
 	} else if !ok { // 没找到该元素，说明该消息是第一次到服务器
-		_, _ = db.SaddCtx(ctx, key, uuid)
+		db.SaddCtx(ctx, key, uuid)
 	}
 	return ok, nil
 }
