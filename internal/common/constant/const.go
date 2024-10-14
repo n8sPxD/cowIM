603105a0e58c583cb1174c18f18db2be5c73371e
n8sPxD <noobsoap233@gmail.com>
2024-10-14 14:00:43 +0800

    docs: 更新 前端设计.md

--- internal/common/constant/const.go
+++ internal/common/constant/const.go
@@ -2,48 +2,48 @@ package constant
 
 // Message 类型
 const (
-	_ = iota
-	MSG_COMMON_MSG
-	MSG_REPLY_MSG
-	MSG_IMAGE_MSG
-	MSG_VIDEO_MSG
-	MSG_FILE_MSG
+	_              = iota
+	MSG_COMMON_MSG // 一般消息
+	MSG_REPLY_MSG  // 回复消息
+	MSG_IMAGE_MSG  // 图片消息
+	MSG_VIDEO_MSG  // 视频消息
+	MSG_FILE_MSG   // 文件消息
 )
 
 // 系统相关 Message 类型
 const (
-	MSG_SYSTEM_MSG = 100 + iota
-	MSG_ACK_MSG    // 消息重传中使用
+	MSG_SYSTEM_MSG = 100 + iota // 暂时不知道用来干嘛
+	MSG_ACK_MSG                 // 消息重传中使用
 )
 
 // 群聊或者单聊
 const (
-	_ = iota
-	SINGLE_CHAT
-	GROUP_CHAT
-	BIG_GROUP_CHAT
-	SYSTEM_INFO = 99
+	_              = iota
+	SINGLE_CHAT         // 单聊
+	GROUP_CHAT          // 群聊
+	BIG_GROUP_CHAT      // 大规模群聊
+	SYSTEM_INFO    = 99 // 系统聊天，一般和USER_SYSTEM捆绑使用
 )
 
 // 存表中区分表的类型
 const (
-	_ = iota
-	MESSAGE_RECORD
-	MESSAGE_SYNC
-	USER_TIMELINE
+	_              = iota
+	MESSAGE_RECORD // 消息记录表
+	MESSAGE_SYNC   // 消息同步表，没啥用
+	USER_TIMELINE  // 用户Timeline表
 )
 
 // 特殊用户
 const (
 	_           = iota
 	USER_SYSTEM // 系统消息
-	USER_COMMON
+	USER_COMMON // 普通用户
 )
 
 // 群聊权限
 const (
-	_ = iota
-	GROUP_MASTER
-	GROUP_ADMIN
-	GROUP_COMMON
+	_            = iota
+	GROUP_MASTER // 群主
+	GROUP_ADMIN  // 管理
+	GROUP_COMMON // 狗群员
 )
