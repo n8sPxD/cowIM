1b982e725888179782cfe447674db87e546f68c7
n8sPxD <noobsoap233@gmail.com>
2024-10-15 19:11:04 +0800

    refactor: 修改部分代码结构

--- internal/im_server/internal/server/messages.go
+++ internal/im_server/internal/server/messages.go
@@ -25,30 +25,16 @@ func (s *Server) sendMessageToBackend() error {
 }
 
 func (s *Server) readMessageFromFrontend(id uint32) error {
-	// 此处的消息为protobuf序列化后的消息
-	logx.Debug("[readMessageFromFrontend] Reading message from user ", id)
 	for {
 		msg, err := s.Manager.ReadMessage(id)
 		if err != nil {
 			// 用户断线
-			if websocket.IsCloseError(
-				err,
-				websocket.CloseNormalClosure,
-				websocket.CloseGoingAway,
-				websocket.CloseAbnormalClosure,
-			) {
+			if websocket.IsCloseError(err, websocket.CloseGoingAway) {
 				logx.Infof("[readMessageFromFrontend] User %d disconnected", id)
 			} else {
 				// 用户无法读取信息，不是因为用户断开了，可能是服务器的问题
-				logx.Errorf(
-					"[readMessageFromFrontend] Read message from user %v failed, error: %v",
-					id,
-					err,
-				)
+				logx.Errorf("[readMessageFromFrontend] Read message from user %v failed, error: %v", id, err)
 			}
-			// 读不了用户的消息，说明后续也不能和用户进行通讯了，所以就和用户断开连接
-			logx.Infof("[readMessageFromFrontend] Removing user %d from redis router status...", id)
-
 			return err
 		}
 		if string(msg) == "ping" {
