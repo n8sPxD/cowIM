32ae0c8320afac264eebfe9de2ece44d59aa99d4
n8spxd <noobsoap233@gmail.com>
2024-11-20 16:59:26 +0800

    refactor: 修改路由状态存储的方式

--- internal/common/db/myRedis/db_init.go
+++ internal/common/db/myRedis/db_init.go
@@ -1,23 +1,15 @@
 package myRedis
 
 import (
-	"github.com/zeromicro/go-zero/core/bloom"
 	"github.com/zeromicro/go-zero/core/stores/redis"
 )
 
 type DB struct {
 	*redis.Redis
-	blooms map[string]*bloom.Filter
 }
 
 func MustNewRedis(c redis.RedisConf) *DB {
 	return &DB{
-		Redis:  redis.MustNewRedis(c),
-		blooms: make(map[string]*bloom.Filter),
+		Redis: redis.MustNewRedis(c),
 	}
-}
-
-func (db *DB) WithBloom(key string, bits uint) *DB {
-	db.blooms[key] = bloom.New(db.Redis, key, bits)
-	return db
 }
--- internal/common/db/myRedis/router_status.go
+++ internal/common/db/myRedis/router_status.go
@@ -2,56 +2,53 @@ package myRedis
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"strconv"
-	"time"
 )
 
-type RouterStatus struct {
-	WorkID     uint16    `json:"work_id"`
-	LastUpdate time.Time `json:"last_update"`
+func (db *DB) GetUserRouterStatus(ctx context.Context, userID uint32) (string, error) {
+	return db.GetCtx(ctx, fmt.Sprintf("r_%d", userID))
 }
 
-func (db *DB) GetUserRouterStatus(ctx context.Context, userID uint32) (*RouterStatus, error) {
-	res, err := db.HgetCtx(ctx, "router", strconv.FormatInt(int64(userID), 10))
-	if err != nil {
-		return nil, err
-	}
-	var status RouterStatus
-	err = json.Unmarshal([]byte(res), &status)
-	if err != nil {
-		return nil, err
-	}
-	return &status, err
+func (db *DB) UpdateUserRouterStatus(ctx context.Context, userID uint32, workID uint16) error {
+	return db.SetCtx(ctx, fmt.Sprintf("r_%d", userID), strconv.Itoa(int(workID)))
 }
 
-// UpdateUserRouterStatus 路由信息登记
-// Key: user_id			Value: { server_work_id: xxx, last_update: xxx }
-// 用户路由信息，保存用户建立长连接的服务器IP和最后和服务器进行心跳检测的时间
-func (db *DB) UpdateUserRouterStatus(ctx context.Context, userID uint32, workID uint16, timestamp time.Time) error {
-	if val, err := json.Marshal(RouterStatus{
-		WorkID:     workID,
-		LastUpdate: timestamp,
-	}); err != nil {
-		return err
-	} else {
-		return db.HsetCtx(ctx, "router", strconv.FormatInt(int64(userID), 10), string(val))
-	}
+func (db *DB) RemoveUserRouterStatus(ctx context.Context, userID uint32) (int, error) {
+	return db.DelCtx(ctx, fmt.Sprintf("r_%d", userID))
 }
 
-func (db *DB) RemoveUserRouterStatus(ctx context.Context, userID uint32) error {
-	_, err := db.HdelCtx(ctx, "router", strconv.FormatInt(int64(userID), 10))
-	if err != nil {
-		return err
+func (db *DB) RemoveAllUserRouterStatus(ctx context.Context, workerID uint16) error {
+	var cursor uint64
+	for {
+		var (
+			keys []string
+			next uint64
+			err  error
+		)
+		if keys, next, err = db.ScanCtx(ctx, cursor, "r_*", 100); err != nil {
+			return err
+		}
+		for _, key := range keys {
+			if v, err := db.GetCtx(ctx, key); err != nil {
+				if !errors.Is(err, redis.Nil) {
+					return err
+				}
+				continue
+			} else {
+				if v == strconv.Itoa(int(workerID)) {
+					if _, err := db.UnlinkCtx(ctx, key); err != nil {
+						return err
+					}
+				}
+			}
+		}
+		cursor = next
+		if cursor == 0 {
+			break
+		}
 	}
 	return nil
-}
-
-func (db *DB) RemoveAllUserRouterStatus() {
-	table, _ := db.Hgetall("router")
-	slices := make([]string, 0, len(table))
-	for key := range table {
-		slices = append(slices, key)
-	}
-	db.Hdel("router", slices...)
 }
