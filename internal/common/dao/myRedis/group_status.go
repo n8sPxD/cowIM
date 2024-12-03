96ec90014132a3e8c85a6b91eefdce9102064332
n8spxd <noobsoap233@gmail.com>
2024-12-03 23:58:56 +0800

    实现群聊群员redis层逻辑

---
+++ internal/common/dao/myRedis/group_status.go
@@ -0,0 +1,24 @@
+package myRedis
+
+import (
+	"context"
+	"strconv"
+)
+
+func (db *Native) GetGroupMembers(ctx context.Context, id uint32) ([]string, error) {
+	if ids, err := db.SMembers(ctx, strconv.Itoa(int(id))).Result(); err != nil {
+		return []string{}, err
+	} else {
+		return ids, nil
+	}
+}
+
+func (db *Native) AddGroupMembers(ctx context.Context, groupid uint32, userid []uint32) error {
+	_, err := db.SAdd(ctx, strconv.Itoa(int(groupid)), userid).Result()
+	return err
+}
+
+func (db *Native) RemGroupMembers(ctx context.Context, groupid uint32, userid []uint32) error {
+	_, err := db.SRem(ctx, strconv.Itoa(int(groupid)), userid).Result()
+	return err
+}
