de90d9089bed78d25e71a69b4ec94200a56cf994
n8spxd <noobsoap233@gmail.com>
2024-12-03 21:20:41 +0800

    refactor: group rpc方法替换原mysql直接查询

--- internal/msg_forward/internal/logic/msg_forward.go
+++ internal/msg_forward/internal/logic/msg_forward.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"github.com/n8sPxD/cowIM/internal/business/group/rpc/types/groupRpc"
 	"github.com/n8sPxD/cowIM/internal/msg_forward/gossip"
 	"github.com/segmentio/kafka-go/compress"
 	"strconv"
@@ -208,84 +209,23 @@ func (l *MsgForwarder) singleChat(msg *front.Message, protobuf []byte) {
 	l.packageMessageAndSend(protobuf, msg.To, msg.Id, msg.MsgType)
 }
 
-// 群聊处理
+// 群聊处理，主动推
 func (l *MsgForwarder) groupChat(msg *front.Message, protobuf []byte) {
-	// 先获取群里所有成员
-	// TODO: 群组成员加缓存
-	members, err := l.svcCtx.MySQL.GetGroupMemberIDs(l.ctx, uint(*msg.Group))
+	members, err := l.svcCtx.GroupRpc.GetGroupMembers(context.Background(), &groupRpc.GroupMembersRequest{Id: *msg.Group})
 	if err != nil {
-		logx.Error("[groupChat] Get group members from mysql failed, error: ", err)
+		logx.Error("[groupChat] Get group members failed, error: ", err)
+		// TODO: retry
 		return
 	}
-	// TODO: 可以优化，先处理所有消息，然后把对应服务器的消息以切片形式发送，避免重复调用WriteMessages
-	for _, member := range members {
-		// 不用给发消息的人发
-		if member == uint(msg.From) {
+
+	for _, member := range members.Ids {
+		if member == msg.From {
 			continue
 		}
-		// TODO: Redis中用Pipeline一次性查询所有member对应的服务器ID, 直接转发，避免重复的单次查询
-		l.packageMessageAndSend(protobuf, msg.To, msg.Id, msg.MsgType)
+		l.packageMessageAndSend(protobuf, member, msg.Id, msg.MsgType)
 	}
 }
 
-/*
-大型群聊 （大于500人） 客户端定期轮询请求
-客户端登陆，同步最新Timeline，服务端不主动推送大型群聊消息
-客户端定期轮询（1秒），发送请求，服务端返回最新的一条消息
-当客户端点进群聊，发送请求，服务端返回服务端最新消息和客户端最新消息之间的所有消息
-
-客户端刚登陆的时候，假如加入了大规模群组1001，则定期给服务器发送消息：
-
-	{
-		from = A
-		to = SYSTEM
-		group = 1001
-		content = "所有群组的id,用下划线_拼接起来"
-		type = BIG_GROUP
-		msg_type = BIG_GROUP_REQ
-		timestamp = 当前本地所有BIG_GROUP消息内的最新时间戳
-	}
-
-此时服务器收到消息，将当前本地（缓存或数据库）的最新一条记录发过去：
-
-	{
-		from = SYSTEM
-		to =  A
-		group = 1001
-		content = 群聊1001的最新一条消息
-		type = BIG_GROUP
-		msg_type = BIG_GROUP_REQ
-		timestamp = timestamp
-	}
-
-客户端收到后，在前端将最新消息显示出来，但是消息不入库，在客户端在线的时间持续间接性发送。
-当客户端具体点击到一个群之后，前端进行完整的定时轮询，每次向服务器发送消息如下：
-
-	{
-		from = A
-		to = SYSTEM
-		group = 1001
-		content = ""
-		type = BIG_GROUP
-		msg_type = BIG_GROUP_ALL_REQ
-		timestamp = 当前本地所有BIG_GROUP消息内的最新时间戳
-	}
-
-服务器接收到了，然后返回消息：
-
-	{
-		id = 消息本身的id
-		from = SYSTEM
-		to = A
-		group = 1001
-		content = 消息内容
-		type = BIG_GROUP
-		msg_type = MSG_COMMON_MSG
-	}
-
-前端接收到，返回Ack，存数据库，如果有其他群例如1002,那么一样的流程。
-该流程可以优化，例如第一次握手时，客户端以切片形式发送请求
-*/
 func (l *MsgForwarder) bigGroupChat(msg *front.Message) {
 }
 
