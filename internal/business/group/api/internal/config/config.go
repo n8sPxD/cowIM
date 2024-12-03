9476974aa0c6e6de725e32af36227f12caa5c637
n8spxd <noobsoap233@gmail.com>
2024-12-04 01:24:47 +0800

    group restful redis依赖注入

--- internal/business/group/api/internal/config/config.go
+++ internal/business/group/api/internal/config/config.go
@@ -1,10 +1,14 @@
 package config
 
-import "github.com/zeromicro/go-zero/rest"
+import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/rest"
+)
 
 type Config struct {
 	rest.RestConf
 	MySQL struct {
 		DataSource string
 	}
+	RedisConf redis.RedisConf
 }
--- internal/business/group/api/internal/svc/servicecontext.go
+++ internal/business/group/api/internal/svc/servicecontext.go
@@ -3,16 +3,19 @@ package svc
 import (
 	"github.com/n8sPxD/cowIM/internal/business/group/api/internal/config"
 	"github.com/n8sPxD/cowIM/internal/common/dao/myMysql"
+	"github.com/n8sPxD/cowIM/internal/common/dao/myRedis"
 )
 
 type ServiceContext struct {
 	Config config.Config
 	MySQL  *myMysql.DB
+	Redis  *myRedis.Native
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
 		MySQL:  myMysql.MustNewMySQL(c.MySQL.DataSource),
+		Redis:  myRedis.MustNewNativeRedis(c.RedisConf),
 	}
 }
